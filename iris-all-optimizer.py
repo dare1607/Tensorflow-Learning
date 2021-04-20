5954ae9e5d4228a6240c8ee5a000c94530fe308d
tanxiaoyang <2725235803@qq.com>
2021-04-20 16:19:54 +0800

    first commit

---
+++ iris-all-optimizer.py
@@ -0,0 +1,146 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import datasets
+import time
+
+features = datasets.load_iris().data  # 导入iris特征
+labels = datasets.load_iris().target  # 导入iris标签
+
+# 随机打乱数据
+np.random.seed(110)
+np.random.shuffle(features)
+np.random.seed(110)
+np.random.shuffle(labels)
+tf.random.set_seed(110)
+
+# 将打乱后的数据集分割为训练集和测试集，训练集为前120行，测试集为后30行
+features_train = features[:-30]
+features_test = features[-30:]
+labels_train = labels[:-30]
+labels_test = labels[-30:]
+
+features_train = tf.cast(features_train, tf.float32)
+features_test = tf.cast(features_test, tf.float32)
+
+# from_tensor_slices函数使输入特征和标签值一一对应。（把数据集分批次，每个批次batch组数据）
+train_db = tf.data.Dataset.from_tensor_slices((features_train, labels_train)).batch(32)
+test_db = tf.data.Dataset.from_tensor_slices((features_test, labels_test)).batch(32)
+
+w1 = tf.Variable(tf.random.truncated_normal([4, 3], stddev=0.1, seed=1))
+b1 = tf.Variable(tf.random.truncated_normal([3], stddev=0.1, seed=1))
+
+lr = 0.1  # 学习率为0.1
+train_loss_results = []  # 将每轮的loss记录在此列表中，为后续画loss曲线提供数据
+test_acc = []  # 将每轮的acc记录在此列表中，为后续画acc曲线提供数据
+epoch = 500
+loss_all = 0  # 每轮分4个step，loss_all记录四个step生成的4个loss的和
+
+# # SGDM动量参数
+# m_w, m_b = 0, 0
+# beta = 0.9
+###################
+
+# # Adagrad,RMSProp参数
+# v_w, v_b = 0, 0
+# beta = 0.9
+###################
+
+# Adam优化器参数
+m_w, m_b = 0, 0
+v_w, v_b = 0, 0
+beta1, beta2 = 0.9, 0.999
+global_step = 0
+###################
+
+now_time = time.time()
+for epoch in range(epoch):  # 数据集级别的循环，每个epoch循环一次数据集
+    for step, (x_train, y_train) in enumerate(train_db):  # batch级别的循环 ，每个step循环一个batch
+        global_step += 1  # adam
+        with tf.GradientTape() as tape:  # with结构记录梯度信息
+            y = tf.matmul(x_train, w1) + b1  # 神经网络乘加运算
+            y = tf.nn.softmax(y)  # 使输出y符合概率分布（此操作后与独热码同量级，可相减求loss）
+            y_ = tf.one_hot(y_train, depth=3)  # 将标签值转换为独热码格式，方便计算loss和accuracy
+            loss = tf.reduce_mean(tf.square(y_ - y))  # 采用均方误差损失函数mse = mean(sum(y-out)^2)
+            loss_all += loss.numpy()  # 将每个step计算出的loss累加，为后续求loss平均值提供数据，这样计算的loss更准确
+        # 计算loss对各个参数的梯度
+        grads = tape.gradient(loss, [w1, b1])
+
+        # SGD(无momentum)优化器, 梯度下降法
+        # # 实现梯度更新 w1 = w1 - lr * w1_grad    b = b - lr * b_grad
+        # w1.assign_sub(lr * grads[0])  # 参数w1自更新
+        # b1.assign_sub(lr * grads[1])  # 参数b自更新
+
+        # # SGDM(有momentum的SGD)优化，在SGD的基础上增加了一阶动量
+        # # sgd-momentum
+        # m_w = beta * m_w + (1 - beta) * grads[0]
+        # m_b = beta * m_b + (1 - beta) * grads[1]
+        # w1.assign_sub(lr * m_w)
+        # b1.assign_sub(lr * m_b)
+
+        # # 优化器RMSProp
+        # v_w = beta * v_w + (1 - beta) * tf.square(grads[0])
+        # v_b = beta * v_b + (1 - beta) * tf.square(grads[1])
+        # w1.assign_sub(lr * grads[0] / tf.sqrt(v_w))
+        # b1.assign_sub(lr * grads[1] / tf.sqrt(v_b))
+
+        # adam
+        m_w = beta1 * m_w + (1 - beta1) * grads[0]
+        m_b = beta1 * m_b + (1 - beta1) * grads[1]
+        v_w = beta2 * v_w + (1 - beta2) * tf.square(grads[0])
+        v_b = beta2 * v_b + (1 - beta2) * tf.square(grads[1])
+
+        m_w_correction = m_w / (1 - tf.pow(beta1, int(global_step)))
+        m_b_correction = m_b / (1 - tf.pow(beta1, int(global_step)))
+        v_w_correction = v_w / (1 - tf.pow(beta2, int(global_step)))
+        v_b_correction = v_b / (1 - tf.pow(beta2, int(global_step)))
+
+        w1.assign_sub(lr * m_w_correction / tf.sqrt(v_w_correction))
+        b1.assign_sub(lr * m_b_correction / tf.sqrt(v_b_correction))
+
+    # 每个epoch，打印loss信息
+    print("Epoch {}, loss: {}".format(epoch, loss_all/4))
+    train_loss_results.append(loss_all / 4)  # 将4个step的loss求平均记录在此变量中
+    loss_all = 0  # loss_all归零，为记录下一个epoch的loss做准备
+
+    # 测试部分
+    # total_correct为预测对的样本个数, total_number为测试的总样本数，将这两个变量都初始化为0
+    total_correct, total_number = 0, 0
+    for x_test, y_test in test_db:
+        # 使用更新后的参数进行预测
+        y = tf.matmul(x_test, w1) + b1
+        y = tf.nn.softmax(y)
+        pried = tf.argmax(y, axis=1)  # 返回y中最大值的索引，即预测的分类
+        # 将pried转换为y_test的数据类型
+        pried = tf.cast(pried, dtype=y_test.dtype)
+        # 若分类正确，则correct=1，否则为0，将bool型的结果转换为int型
+        correct = tf.cast(tf.equal(pried, y_test), dtype=tf.int32)
+        # 将每个batch的correct数加起来
+        correct = tf.reduce_sum(correct)
+        # 将所有batch中的correct数加起来
+        total_correct += int(correct)
+        # total_number为测试的总样本数，也就是x_test的行数，shape[0]返回变量的行数
+        total_number += x_test.shape[0]
+    # 总的准确率等于total_correct/total_number
+    acc = total_correct / total_number
+    test_acc.append(acc)
+    print("Test_acc:", acc)
+    print("--------------------------")
+total_time = time.time() - now_time
+print('total_time: ', total_time)
+
+# 绘制 loss 曲线
+plt.title('Loss Function Curve')  # 图片标题
+plt.xlabel('Epoch')  # x轴变量名称
+plt.ylabel('Loss')  # y轴变量名称
+plt.plot(train_loss_results, label="$Loss$")  # 逐点画出train_loss_results值并连线，连线图标是Loss
+plt.legend()  # 画出曲线图标
+plt.show()  # 画出图像
+
+# 绘制 Accuracy 曲线
+plt.title('Acc Curve')
+plt.xlabel('Epoch')
+plt.ylabel('Acc')
+plt.plot(test_acc, label="$Accuracy$")
+plt.legend()
+plt.show()
